0c76199d1880be6e12f8d70bd4cad04273caab23
Vivek Singh Rajput <vivekcatqua309@gmail.com>
2023-06-11 12:30:24 +0530

    Update views.py

--- App/views.py
+++ App/views.py
@@ -32,7 +32,7 @@ def mlmodel(request):
         malldistence = int(request.POST['malldistence'])
 
         data = [[type, carpetarea, metrodistence, ecodistence, malldistence]]
-        with open('C:/Users/VIVEK/ReInvest/model1.pkl', 'rb') as file:
+        with open('./model1.pkl', 'rb') as file:
             model = pickle.load(file)
         res = model.predict(data)
 
